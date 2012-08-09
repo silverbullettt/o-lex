f4effca1c6dc07b1dc016e2ebeae6d7d41ccd96e
Silverbullettt <silverbullettt@gmail.com>
2012-08-09 14:55:52 +0800

    add star-closure, and modify make-plain-nfa

--- nfa.rkt
+++ nfa.rkt
@@ -1,15 +1,16 @@
 #lang racket
 
 (require "table.rkt")
-
-(provide make-nfa make-trans)
+(require "utility.rkt")
+(provide make-trans make-nfa make-empty-nfa make-plain-nfa
+         star-closure)
 
 (define (make-trans . tuples)
   (let ((table (make-table 2)))
     (begin
-      (map (λ (tuple)
+      (map (lambda (tuple)
              ((table 'insert!)
-              (car tuple) (cadr tuple) (caddr tuple)))
+              (first tuple) (second tuple) (third tuple)))
            tuples)
       table)))
 
@@ -25,14 +26,15 @@
         ((Δ 'lookup) q sym)
         '()))
   (define (accept? states)
-    (if (null? (filter (λ (s) (member s F)) states))
+    (if (null? (filter (lambda (s) (member s F)) states))
         #f
         #t))
   (define (ε-span states) 
-    ; deal with ε-transfer by span the set of states with which it can reach through 'ε'
+    ; deal with ε-moves by span the set of states with which it can reach through 'ε'
+    ; (printf "   span states: ~a~%" states)
     (remove-duplicates
      (flatten
-      (map (λ (s) (cons s (next-states s #\ε))) states))))
+      (map (lambda (s) (cons s (next-states s #\ε))) states))))
   (define (recognize-iter curr-states str)
     ; (printf "states: ~a, str: ~a~%" curr-states str)
     (cond ((null? curr-states) #f)
@@ -41,9 +43,54 @@
            (let ((first-sym (string-ref str 0))
                  (rest-sym (substring str 1)))
              (for/or ([s curr-states])
+               ; (printf " for/or s: ~a, curr-states: ~a~%" s curr-states)
                (recognize-iter (ε-span (next-states s first-sym)) rest-sym))))))
   (define (recognize str)
     (if (recognize-iter (ε-span (list q0)) str)
         'accept
         'reject))
-  recognize)
+  
+  (define (dispatch m)
+    (case m
+      ['recog recognize]
+      ['s Q]
+      ['alphabet Σ]
+      ['T Δ]
+      ['init q0]
+      ['F F]))
+  dispatch)
+
+(define (make-empty-nfa . q0)
+  ; make a NFA that can recognize empty string
+  (let ((q (if (null? q0) 0 (car q0))))
+    (make-nfa q '() (make-trans) q (list q))))
+
+(define (make-plain-nfa word)
+  ; make a NFA which can recognize the word
+  ; the states of result are presented by 0~length
+  (if (= (string-length word) 0)
+      (make-empty-nfa)
+      (let ((Q (range (string-length word)))
+            (alphabet (remove-duplicates (string->list word)))
+            (t (make-table 2))
+            (init 0)
+            (F (list (string-length word))))
+        (begin
+          (for-each
+           (lambda (s) ((t 'insert!) s (string-ref word s) (list (+ s 1))))
+           (range (string-length word)))
+          (make-nfa Q alphabet t init F)))))
+
+(define (star-closure nfa)
+  (let ((t (nfa 'T))
+        (init (nfa 'init))
+        (F (nfa 'F)))
+    (begin
+      ((t 'insert!) init #\ε (car F))
+      (for-each (lambda (s) ; acceptable state
+                  (let ((new-next (if ((t 'lookup) s #\ε)
+                                      (cons init ((t 'lookup) s #\ε))
+                                      (list init))))
+                    ((t 'insert!) s #\ε new-next)))
+                F)
+      (make-nfa (nfa 'Q) (nfa 'alphabet) t init F))))
---
+++ utility.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(provide range accumulate)
+
+(define (accumulate op initial seq)
+  (if (null? seq)
+      initial
+      (op (car seq) (accumulate op initial (cdr seq)))))
+
+(define (range . params)
+  (define (check-params start end step)
+    (cond ((and (positive? step) (< end start))
+           (error "range: step > 0 && start > end"))
+          ((and (negative? step) (> end start))
+           (error "range: step < 0 && start < end"))
+          ((zero? step)
+           (error "range: step cannot be zero!"))
+          (else 'ok)))
+  (define (range-iter start end step lst)
+    (if (or (and (> step 0) (>= start end))
+            (and (< step 0) (<= start end)))
+        (reverse lst)
+        (range-iter (+ start step) end step (cons start lst))))
+  (define (check-and-iter start end step)
+    (if (eq? (check-params start end step) 'ok)
+        (range-iter start end step '())
+        #f))
+  
+  (case (length params)
+    ['1 (check-and-iter 0 (first params) 1)]
+    ['2 (check-and-iter (first params) (second params) 1)]
+    ['3 (check-and-iter (first params) (second params) (third params))]
+    [else (error "range: params must be (range end) or (range start end [step])")]))
